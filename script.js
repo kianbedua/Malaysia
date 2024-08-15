a21372b764fc6f239782abbc5ebf36e5ad1f5bac
kianbedua <kyaaanerldbedua@gmail.com>
2024-08-16 02:42:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var type = new Typed(".text",{
+    strings:["Kuala Lumpur", "Friendly People", "Petronas Twin Tower", "Vibrant Culture", "Merdeka Tower"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+}); 
